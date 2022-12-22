e56be2cc4ed3c40fa048098ec723b062f80f6d70
buithean2010 <buithean2010@yahoo.com>
2022-12-22 16:17:26 +0900

    commit

---
+++ Week 1/test/test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Bui The An on 2022/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(content: {
+            RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 3)
+            Text("Hello, world!")
+        }).foregroundColor(Color.red)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
